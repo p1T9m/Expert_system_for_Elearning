7108074af47715b5e0d0d7ae3f3ae365b13f6c56
p1T9m <tahamohammad2002@gmail.com>
2023-07-17 21:29:44 +0800

    second commit

--- TMT/TMT/settings.py
+++ TMT/TMT/settings.py
@@ -134,6 +134,13 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "phphelper12@gmail.com"
+EMAIL_HOST_PASSWORD = "jycdomxvvedlhvlw"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- TMT/TMT/urls.py
+++ TMT/TMT/urls.py
@@ -13,6 +13,18 @@ urlpatterns = [
     path('profile/',user_view.profile, name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html')
+         , name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html')
+         , name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html')
+         , name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html')
+         , name='password_reset_complete'),
     path('expert-system/', views.expert_system, name='expert_system'),
     path('beginner-page/', views.beginner_page, name='beginner_page'),
     path('intermediate-page/', views.intermediate_page, name='intermediate_page'),
