2d456e3c7547bc141e6c0d613a9033341bb47555
p1T9m <tahamohammad2002@gmail.com>
2023-07-21 02:23:53 +0800

    7

--- TMT/core/views.py
+++ TMT/core/views.py
@@ -13,5 +13,29 @@ def course(request):
 def intro(request):
     return render(request, 'core/c++intro.html')
 
+def syntax(request):
+    return render(request, 'core/c++syntax.html')
+
+def varaibles(request):
+    return render(request, 'core/c++varaibles.html')
+
+def pintro(request):
+    return render(request, 'core/pythonintro.html')
+
+def psyntax(request):
+    return render(request, 'core/pythonsyntax.html')
+
+def pvariables(request):
+    return render(request, 'core/pythonvariable.html')
+
+def jintro(request):
+    return render(request, 'core/Javaintro.html')
+
+def jsyntax(request):
+    return render(request, 'core/Javasyntax.html')
+
+def jvariables(request):
+    return render(request, 'core/Javavar.html')
+
 
 
